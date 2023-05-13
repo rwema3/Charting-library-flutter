0bd4ce7a1e87604d442cd514df448423b4d3e8e6
rwemadominik3@gmail.com <52289151+rwema3@users.noreply.github.com>
2023-05-13 23:49:51 +0200

    writeNotNull

--- flutter_app/lib/components/tvchart/tvchart_types.g.dart
+++ flutter_app/lib/components/tvchart/tvchart_types.g.dart
@@ -43,3 +43,55 @@ Map<String, dynamic> _$BarToJson(Bar instance) {
     if (value != null) {
       val[key] = value;
     }
+  }
+
+  writeNotNull('volume', instance.volume);
+  return val;
+}
+
+Exchange _$ExchangeFromJson(Map<String, dynamic> json) => Exchange(
+      value: json['value'] as String,
+      name: json['name'] as String,
+      desc: json['desc'] as String,
+    );
+
+Map<String, dynamic> _$ExchangeToJson(Exchange instance) => <String, dynamic>{
+      'value': instance.value,
+      'name': instance.name,
+      'desc': instance.desc,
+    };
+
+DatafeedConfiguration _$DatafeedConfigurationFromJson(
+        Map<String, dynamic> json) =>
+    DatafeedConfiguration(
+      exchanges: (json['exchanges'] as List<dynamic>?)
+          ?.map((e) => Exchange.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      supportedResolutions: (json['supported_resolutions'] as List<dynamic>?)
+          ?.map((e) => e as String)
+          .toList(),
+      currencyCodes: (json['currency_codes'] as List<dynamic>?)
+          ?.map((e) => e as String)
+          .toList(),
+      supportsMarks: json['supports_marks'] as bool?,
+      supportsTime: json['supports_time'] as bool?,
+      supportsTimescaleMarks: json['supports_timescale_marks'] as bool?,
+      symbolsTypes: (json['symbols_types'] as List<dynamic>?)
+          ?.map((e) => DatafeedSymbolType.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$DatafeedConfigurationToJson(
+    DatafeedConfiguration instance) {
+  final val = <String, dynamic>{};
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('exchanges', instance.exchanges);
+  writeNotNull('supported_resolutions', instance.supportedResolutions);
+  writeNotNull('currency_codes', instance.currencyCodes);
+  writeNotNull('supports_marks', instance.supportsMarks);
